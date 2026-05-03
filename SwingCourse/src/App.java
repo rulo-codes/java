da59d0dffa571c6fe53892caadce924b94ec00ba
rulo-codes <150222307+rulo-codes@users.noreply.github.com>
2026-05-03 19:01:54 +0800

    Learn SwingCourse - Panels and Layouts

--- SwingCourse/src/App.java
+++ SwingCourse/src/App.java
@@ -1,64 +1,53 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Image;
-
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.border.Border;
+import javax.swing.JPanel;
+import java.awt.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Set Frame
-        JFrame frame = new JFrame();
+        ImageIcon icon = new ImageIcon(App.class.getResource("./asset/thumbs-up.png"));
+        // Resize Icon
+        Image iconImage = icon.getImage(); // Turn icon to image
+        Image resizeIconImage = iconImage.getScaledInstance(100, 100, Image.SCALE_SMOOTH); // Resize Image
 
-        // Change Window Icon
-        ImageIcon imageIcon = new ImageIcon(App.class.getResource("/asset/logo.png"));
-        frame.setIconImage(imageIcon.getImage());
+        // Turn image to icon
+        ImageIcon imageIcon = new ImageIcon(resizeIconImage);
 
-        // Add Background
-        frame.getContentPane().setBackground(new Color(200, 200, 200));
-
-        // Add Label
         JLabel label = new JLabel();
-        label.setText("Bro, just code and vibe!");
-        frame.add(label);
+        label.setText("HELLO!");
+        label.setIcon(imageIcon);
+        label.setVerticalAlignment(JLabel.BOTTOM);
+        label.setHorizontalAlignment(JLabel.RIGHT);
+        label.setBounds(0, 0, 75, 75); // if layout is null
 
-        // Add Image
-        ImageIcon imageCat = new ImageIcon(App.class.getResource("/asset/logo.png"));
-        Image iconImage = imageCat.getImage(); // Turn icon to image
-        Image resizeIconImage = iconImage.getScaledInstance(150, 150, Image.SCALE_SMOOTH); // Resize Image
-        ImageIcon imageCatIcon = new ImageIcon(resizeIconImage); // Turn image to icon
-        label.setIcon(imageCatIcon); // Set icon to text
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0, 0, 250, 250);
 
-        // Position Text and Image
-        label.setHorizontalTextPosition(JLabel.CENTER);// Horizontal Text Position
-        label.setVerticalTextPosition(JLabel.TOP);// Vertical Text Position
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250, 0, 250, 250);
 
-        // Style Text Label
-        label.setForeground(Color.red); // Set Text Color
-        label.setFont(new Font("Roboto", Font.PLAIN, 20)); // Set Font Family
-        label.setIconTextGap(30);// Set gap between Text and Icon
-        label.setBackground(Color.white);// Set Label Background Color
-        label.setOpaque(true);// Display Label Background
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0, 250, 500, 250);
+        // Border Layout
+        greenPanel.setLayout(new BorderLayout());
 
-        // Set Label Border
-        Border border = BorderFactory.createLineBorder(Color.red, 3);
-        label.setBorder(border);
-        label.setVerticalAlignment(JLabel.CENTER);// Vertical Position of Label + Image
-        label.setHorizontalAlignment(JLabel.CENTER);// Horizontal Position of Label + Image
-        label.setBounds(100, 100, 300, 300);// Set X and Y boundary of Border
-
-        // Set Frame Properties
-        frame.setTitle("Java Swing Course");
+        // Setup JFrame
+        JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setSize(600, 600); // ensure explicit size when using null layout
+        frame.setLayout(null);
+        frame.setSize(750, 750);
+        frame.add(redPanel);
         frame.setVisible(true);
-        frame.pack();
+        // Add the panels, etc.
+        greenPanel.add(label);
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);
     }
 }
 
-// TImestamp: 30:06
+// TImestamp: 42:30
